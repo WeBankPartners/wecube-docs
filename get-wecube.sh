867fa7cc483abc2830a8c3487acc66b7b7f14da7
Tong Zhang <tz.tong.zhang@hotmail.com>
2020-07-18 13:01:28 +0800

    switch to latest installer script

--- get-wecube.sh
+++ get-wecube.sh
@@ -32,13 +32,11 @@ echo ""
 read -p "Continue? [y/Y] " -n 1 -r && echo ""
 [[ ! $REPLY =~ ^[Yy]$ ]] && echo "Installation aborted." && exit 1
 
-
-BASE_DIR="$dest_dir/installer"
-INSTALLER_DIR="$BASE_DIR/wecube"
-mkdir -p "$INSTALLER_DIR"
+mkdir -p $dest_dir
 
 INSTALLER_URL="https://github.com/kanetz/delivery-by-terraform/archive/master.zip"
-INSTALLER_PKG="$BASE_DIR/wecube-installer.zip"
+INSTALLER_PKG="$dest_dir/wecube-installer.zip"
+INSTALLER_DIR="$dest_dir/installer"
 echo -e "\nFetching WeCube installer from $INSTALLER_URL"
 RETRIES=30
 while [ $RETRIES -gt 0 ]; do
@@ -53,13 +51,148 @@ while [ $RETRIES -gt 0 ]; do
 done
 [ $RETRIES -eq 0 ] && echo 'Failed to fetch installer package! Installation aborted.' && exit 1
 
-unzip -o -q $INSTALLER_PKG -d $BASE_DIR
-cp -R "$BASE_DIR/delivery-by-terraform-master/delivery-wecube-for-stand-alone/application-for-tencentcloud/wecube" $BASE_DIR
-[ -f $wecube_version ] && cp $wecube_version $INSTALLER_DIR
+unzip -o -q $INSTALLER_PKG -d $dest_dir
+cp -R "$dest_dir/delivery-by-terraform-master/installer" $dest_dir
+[ -f $wecube_version ] && \
+  cp $wecube_version "$INSTALLER_DIR/wecube-platform/" && \
+  cp $wecube_version "$INSTALLER_DIR/wecube-system-settings/"
 
 echo -e "\nRunning WeCube installer scripts...\n"
 pushd $INSTALLER_DIR >/dev/null
-./install-wecube.sh $install_target_host $mysql_password $wecube_version $dest_dir 'Y'
+
+PROVISIONING_ENV_FILE="$INSTALLER_DIR/provisioning.env"
+cat <<EOF >"$PROVISIONING_ENV_FILE"
+DATE_TIME='$(date --rfc-3339=seconds)'
+HOST_PRIVATE_IP=${install_target_host}
+WECUBE_HOME=${dest_dir}
+
+DOCKER_PORT=2375
+
+S3_PORT=9000
+S3_ACCESS_KEY=access_key
+S3_SECRET_KEY=secret_key
+
+MYSQL_PORT=3307
+MYSQL_USERNAME=root
+MYSQL_PASSWORD=${mysql_password_default}
+EOF
+./invoke-installer.sh "$PROVISIONING_ENV_FILE" docker mysql-docker minio-docker open-monitor-agent
+
+WECUBE_DB_ENV_FILE="$INSTALLER_DIR/db-deployment-wecube-db-standalone.env"
+cat <<EOF >"$WECUBE_DB_ENV_FILE"
+DATE_TIME='$(date --rfc-3339=seconds)'
+HOST_PRIVATE_IP=${install_target_host}
+
+DB_HOST=${install_target_host}
+DB_PORT=3307
+DB_NAME=wecube
+DB_USERNAME=root
+DB_PASSWORD=${mysql_password_default}
+EOF
+./invoke-installer.sh "$WECUBE_DB_ENV_FILE" db-connectivity
+
+AUTH_SERVER_DB_ENV_FILE="$INSTALLER_DIR/db-deployment-auth-server-db-standalone.env"
+cat <<EOF >"$AUTH_SERVER_DB_ENV_FILE"
+DATE_TIME='$(date --rfc-3339=seconds)'
+HOST_PRIVATE_IP=${install_target_host}
+
+DB_HOST=${install_target_host}
+DB_PORT=3307
+DB_NAME=auth_server
+DB_USERNAME=root
+DB_PASSWORD=${mysql_password_default}
+EOF
+./invoke-installer.sh "$AUTH_SERVER_DB_ENV_FILE" db-connectivity
+
+WECUBE_PLATFORM_ENV_FILE="$INSTALLER_DIR/app-deployment-wecube-platform-standalone.env"
+cat <<EOF >"$WECUBE_PLATFORM_ENV_FILE"
+DATE_TIME='$(date --rfc-3339=seconds)'
+HOST_PRIVATE_IP=${install_target_host}
+WECUBE_HOME=${dest_dir}
+WECUBE_RELEASE_VERSION=${wecube_version}
+SHOULD_INSTALL_PLUGINS=true
+INITIAL_PASSWORD=${mysql_password_default}
+
+STATIC_RESOURCE_HOSTS=${install_target_host}
+S3_HOST=${install_target_host}
+
+CORE_DB_HOST=${install_target_host}
+CORE_DB_PORT=3307
+CORE_DB_NAME=wecube
+CORE_DB_USERNAME=root
+CORE_DB_PASSWORD=${mysql_password_default}
+
+AUTH_SERVER_DB_HOST=${install_target_host}
+AUTH_SERVER_DB_PORT=3307
+AUTH_SERVER_DB_NAME=auth_server
+AUTH_SERVER_DB_USERNAME=root
+AUTH_SERVER_DB_PASSWORD=${mysql_password_default}
+
+PLUGIN_DB_HOST=${install_target_host}
+PLUGIN_DB_PORT=3307
+PLUGIN_DB_NAME=mysql
+PLUGIN_DB_USERNAME=root
+PLUGIN_DB_PASSWORD=${mysql_password_default}
+EOF
+./invoke-installer.sh "$WECUBE_PLATFORM_ENV_FILE" wecube-platform
+
+WECUBE_PLUGIN_HOSTING_ENV_FILE="$INSTALLER_DIR/app-deployment-wecube-plugin-hosting-standalone.env"
+cat <<EOF >"$WECUBE_PLUGIN_HOSTING_ENV_FILE"
+DATE_TIME='$(date --rfc-3339=seconds)'
+HOST_PRIVATE_IP=${install_target_host}
+WECUBE_HOME=${dest_dir}
+WECUBE_RELEASE_VERSION=${wecube_version}
+SHOULD_INSTALL_PLUGINS=true
+INITIAL_PASSWORD=${mysql_password_default}
+
+CORE_HOST=${install_target_host}
+
+CORE_DB_HOST=${install_target_host}
+CORE_DB_PORT=3307
+CORE_DB_NAME=wecube
+CORE_DB_USERNAME=root
+CORE_DB_PASSWORD=${mysql_password_default}
+EOF
+./invoke-installer.sh "$WECUBE_PLUGIN_HOSTING_ENV_FILE" wecube-plugin-hosting
+
+WECUBE_SYSTEM_SETTINGS_ENV_FILE="$INSTALLER_DIR/app-deployment-wecube-system-settings-standalone.env"
+cat <<EOF >"$WECUBE_SYSTEM_SETTINGS_ENV_FILE"
+DATE_TIME='$(date --rfc-3339=seconds)'
+HOST_PRIVATE_IP=${install_target_host}
+WECUBE_HOME=${dest_dir}
+WECUBE_RELEASE_VERSION=${wecube_version}
+SHOULD_INSTALL_PLUGINS=true
+INITIAL_PASSWORD=${mysql_password_default}
+
+S3_ACCESS_KEY=access_key
+S3_SECRET_KEY=secret_key
+AGENT_S3_BUCKET_NAME=wecube-agent
+
+CORE_HOST=${install_target_host}
+S3_HOST=${install_target_host}
+PLUGIN_HOST=${install_target_host}
+PORTAL_HOST=${install_target_host}
+
+CORE_DB_HOST=${install_target_host}
+CORE_DB_PORT=3307
+CORE_DB_NAME=wecube
+CORE_DB_USERNAME=root
+CORE_DB_PASSWORD=${mysql_password_default}
+
+AUTH_SERVER_DB_HOST=${install_target_host}
+AUTH_SERVER_DB_PORT=3307
+AUTH_SERVER_DB_NAME=auth_server
+AUTH_SERVER_DB_USERNAME=root
+AUTH_SERVER_DB_PASSWORD=${mysql_password_default}
+
+PLUGIN_DB_HOST=${install_target_host}
+PLUGIN_DB_PORT=3307
+PLUGIN_DB_NAME=mysql
+PLUGIN_DB_USERNAME=root
+PLUGIN_DB_PASSWORD=${mysql_password_default}
+EOF
+./invoke-installer.sh "$WECUBE_SYSTEM_SETTINGS_ENV_FILE" wecube-system-settings
+
 popd >/dev/null
 
 echo -e "\n\nWeCube installation completed. Please visit WeCube at http://${install_target_host}:19090\n"
